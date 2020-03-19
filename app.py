2b499b64a84d9232e086ab54d1429a89ca7876cb
Priscila Portela <priscila.portela.c@gmail.com>
2020-03-19 11:30:24 -0300

    inserindo rotas

---
+++ app.py
@@ -0,0 +1,22 @@
+# app.py
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template("index.html")
+
+@app.route("/formulario")
+def shoelaces():
+    return 'direcionar para link de formlulario para insercao de novos negócios'
+
+@app.route("/sobre nós")
+def about():
+    return 'Inserir link para twitter, intagram...'
+
+@app.route('/como-obter-lat-lng')
+    return 'Énsinar as pessoas como obter a latitude e longitude do seu lugar preferido'
+
+if __name__ == '__main__':
+    app.run(debug=True)
