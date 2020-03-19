a510750139be1f10265dac7c893988da52bc2288
Priscila Portela <priscila.portela.c@gmail.com>
2020-03-19 12:16:08 -0300

    abrir nova aba

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # app.py
 from flask import Flask, render_template, redirect
+import webbrowser
 
 app = Flask(__name__)
 
@@ -9,15 +10,15 @@ def hello():
 
 @app.route('/formulario')
 def form():
-    return redirect('https://forms.gle/7KWhBWsm2nFJqMFA9', code=302)
+    return webbrowser.open_new_tab('https://forms.gle/7KWhBWsm2nFJqMFA9')
 
 @app.route('/sobre')
 def about():
-    return 'Inserir link para twitter, intagram...'
+    return 'Inserir link para twitter e github...'
 
 @app.route('/como-obter-lat-lng')
 def info():
-    return redirect("https://support.google.com/maps/answer/18539?co=GENIE.Platform%3DDesktop&hl=pt-BR", code=302)
+    return webbrowser.open_new_tab('https://support.google.com/maps/answer/18539?co=GENIE.Platform%3DDesktop&hl=pt-BR')
 
 if __name__ == '__main__':
     app.run(debug=True)
